c4632535cc50ed8281626bee77ea4269e16b156e
Odame <kwesi.danquah@daaseng.com>
2023-08-02 18:44:30 +0100

    feat - [answer options for questions]

--- app/Models/QuestionOption.php
+++ app/Models/QuestionOption.php
@@ -4,17 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class Question extends Model
+class QuestionOption extends Model
 {
     use HasFactory,SoftDeletes;
 
     protected $guarded = ['id'];
 
-    public function questionOptions(): HasMany
+    protected $casts = [
+        'correct' => 'boolean',
+    ];
+
+    public function question(): BelongsTo
     {
-        return $this->hasMany(QuestionOption::class)->inRandomOrder();
+        return $this->belongsTo(Question::class);
     }
 }
